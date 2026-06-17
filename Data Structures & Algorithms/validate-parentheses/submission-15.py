6a2f67629ca5b6a0d796cfa3ed52731e1ecee01f
BluejayWagon <64602692+BluejayWagon@users.noreply.github.com>
2026-06-17 06:10:42 -0600

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        opStack = []
+        parenHash = {')':'(','}':'{',']':'['}
+
+        for i in s:
+            if i in parenHash:
+                if not opStack:
+                    return False
+                op = opStack.pop()
+                if parenHash[i] != op:
+                    return False
+            else:
+                opStack.append(i)
+        return not opStack
